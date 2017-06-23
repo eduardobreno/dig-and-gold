58094ac658c1f304fbd49b0b85068fd6e4a34362
Aecio Braga <aecio.braga@tcl.com>
2017-06-23 09:04:24 -0400

    correção tela de creditos e botoes

---
+++ Assets/Scripts/AutoLevelTransition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AutoLevelTransition : MonoBehaviour {
+
+	public int waitTime = 2;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("AutoChangeLevel");
+	}
+	
+	IEnumerator AutoChangeLevel(){
+		yield return new WaitForSeconds (waitTime);
+		float fadeTime = this.GetComponent<Fading>().BeginFade(1);
+		yield return new WaitForSeconds (fadeTime);
+		SceneManager.LoadScene(Application.loadedLevel + 1);
+
+	}
+
+}
